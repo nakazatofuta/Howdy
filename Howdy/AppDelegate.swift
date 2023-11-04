c571ec44b1057dd063b091d2c6d798e7323361de
nakazatofuta <futanakazato@gmail.com>
2023-11-05 00:23:22 +0900

    [ADD]キーボード関連の処理を追加

--- Howdy/AppDelegate.swift
+++ Howdy/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 中里楓太 on 2023/10/12.
 //
 
+import IQKeyboardManagerSwift
 import UIKit
 
 @main
@@ -12,11 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         // NavigationBarフォント設定
         for controlState in [UIControl.State.normal, UIControl.State.disabled, UIControl.State.focused, UIControl.State.highlighted, UIControl.State.selected] {
             UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Corporate-Logo-Medium-ver3", size: 18)!], for: controlState)
         }
-        
+
         if true {
             // windowを宣言
             self.window = UIWindow(frame: UIScreen.main.bounds)
