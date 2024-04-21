cfd95050a1d1df101959f2e10439d2ea9d531d5a
nakazatofuta <futanakazato@gmail.com>
2024-04-21 18:10:27 +0900

    [MOD]UserDefaultでプロフィール画像が保持されるように変更

--- Howdy/ViewModels/TopViewModel.swift
+++ Howdy/ViewModels/TopViewModel.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class TopViewModel {
     let userModel = UserModel()
+    
+    let profileImageKey = "profileImageKey"
+    
+    var profileImage: UIImage {
+        get {
+            return UserDefaults.standard.object(forKey: profileImageKey) as! UIImage
+        }
+        set {
+            return UserDefaults.standard.set(DestinationUserInfo.profileImage, forKey: profileImageKey)
+        }
+    }
 
     func fetchScenResult() -> String {
         return DestinationUserInfo.destinationId
